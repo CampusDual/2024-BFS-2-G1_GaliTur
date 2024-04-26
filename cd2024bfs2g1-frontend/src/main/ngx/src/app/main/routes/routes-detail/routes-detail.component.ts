35fd74b01b0a2fe07bd0c34c5e64a31387b6ef1f
Simón Souto Dopazo <simonsoutodopazo@gmail.com>
2024-04-26 09:28:29 +0200

    Actualización de Route-home y Route-detail

--- cd2024bfs2g1-frontend/src/main/ngx/src/app/main/routes/routes-detail/routes-detail.component.ts
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/main/routes/routes-detail/routes-detail.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-routes-detail',
   templateUrl: './routes-detail.component.html',
   styleUrls: ['./routes-detail.component.css']
 })
-export class RoutesDetailComponent {
+export class RoutesDetailComponent implements OnInit {
 
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    protected sanitizer: DomSanitizer
+  ) { }
+
+  public getImageSrc(base64: any): any {
+    return base64 ? this.sanitizer.bypassSecurityTrustResourceUrl('data:image/*;base64,' + base64.bytes) : './assets/images/no-image-transparent.png';
+  }
+
+  ngOnInit() {
+  }
 }
+
--- cd2024bfs2g1-frontend/src/main/ngx/src/app/main/routes/routes-home/routes-home.component.ts
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/main/routes/routes-home/routes-home.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DomSanitizer } from '@angular/platform-browser';
+import { RoutesDetailComponent } from '../routes-detail/routes-detail.component';
 
 @Component({
   selector: 'app-routes-home',
   templateUrl: './routes-home.component.html',
   styleUrls: ['./routes-home.component.css']
 })
-export class RoutesHomeComponent {
+export class RoutesHomeComponent implements OnInit {
+
+  constructor(
+    protected dialog: MatDialog,
+    protected sanitizer: DomSanitizer
+  ) { }
+
+  ngOnInit() {
+  }
+
+  public getImageSrc(base64: any): any {
+    return base64 ? this.sanitizer.bypassSecurityTrustResourceUrl('data:image/*;base64,' + base64.bytes) : './assets/images/no-image-transparent.png';
+  }
+
+  public openDetail(data: any): void {
+    this.dialog.open(RoutesDetailComponent, {
+      height: '330px',
+      width: '520px',
+      data: data
+    });
+  }
+
 
 }
