1f223f7a83d8cff56764123fb2db1eb435ae9b0d
Daniel Verdes Martinez <dverdesm@gmail.com>
2024-04-29 14:46:20 +0200

    AgencyGuide Service - CRUD

--- cd2024bfs2g1-model/src/main/java/com/campusdual/cd2024bfs2g1/model/core/service/business/AgencyGuideService.java
+++ cd2024bfs2g1-model/src/main/java/com/campusdual/cd2024bfs2g1/model/core/service/business/AgencyGuideService.java
@@ -2,6 +2,7 @@ package com.campusdual.cd2024bfs2g1.model.core.service.business;
 
 import com.campusdual.cd2024bfs2g1.api.core.service.business.IAgencyGuideService;
 import com.campusdual.cd2024bfs2g1.model.core.dao.business.AgencyGuideDao;
+import com.campusdual.cd2024bfs2g1.model.core.dao.business.BusinessDao;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -22,6 +25,9 @@ public class AgencyGuideService implements IAgencyGuideService {
     @Autowired
     private AgencyGuideDao agencyGuideDao;
 
+    @Autowired
+    private BusinessDao businessDao;
+
     @Override
     public EntityResult agencyGuideQuery(Map<String, Object> keysValues, List<String> attributes) throws OntimizeJEERuntimeException {
         return this.daoHelper.query(this.agencyGuideDao,keysValues, attributes);
@@ -29,16 +35,75 @@ public class AgencyGuideService implements IAgencyGuideService {
 
     @Override
     public EntityResult agencyGuideInsert(Map<String, Object> keysValues) throws OntimizeJEERuntimeException {
-        return this.daoHelper.insert(this.agencyGuideDao, keysValues);
+
+        Map<String,Object> agencyMap = new HashMap<>(keysValues);
+        agencyMap.remove("merchant_id");
+        agencyMap.remove("bsn_name");
+        agencyMap.remove("bsn_type");
+        agencyMap.remove("bsn_description");
+        agencyMap.remove("bsn_cif");
+        agencyMap.remove("bsn_address");
+        agencyMap.remove("bsn_phone");
+        agencyMap.remove("bsn_email");
+        agencyMap.remove("bsn_photos");
+        agencyMap.remove("bsn_website");
+        agencyMap.remove("bsn_schedule");
+
+        EntityResult er = this.daoHelper.insert(this.businessDao, keysValues);
+
+        agencyMap.put("bsn_id",(int) er.get("bsn_id"));
+
+
+        return this.daoHelper.insert(this.agencyGuideDao, agencyMap);
     }
 
     @Override
     public EntityResult agencyGuideUpdate(Map<String, Object> attributesValues, Map<String, Object> keysValues) throws OntimizeJEERuntimeException {
-        return this.daoHelper.update(this.agencyGuideDao, attributesValues, keysValues);
+
+        Map<String,Object> agencyMap = new HashMap<>(attributesValues);
+        agencyMap.remove("merchant_id");
+        agencyMap.remove("bsn_name");
+        agencyMap.remove("bsn_type");
+        agencyMap.remove("bsn_description");
+        agencyMap.remove("bsn_cif");
+        agencyMap.remove("bsn_address");
+        agencyMap.remove("bsn_phone");
+        agencyMap.remove("bsn_email");
+        agencyMap.remove("bsn_photos");
+        agencyMap.remove("bsn_website");
+        agencyMap.remove("bsn_schedule");
+
+        List <String> qKeys = new ArrayList<String>();
+        qKeys.add("B.bsn_id");
+        EntityResult erAgencyGuide = this.daoHelper.query(this.agencyGuideDao,keysValues, qKeys);
+        Map<String,Object> mapBsn = new HashMap<>();
+
+        ArrayList<Integer> al = (ArrayList<Integer>) erAgencyGuide.get("bsn_id");
+        int id = al.get(0);
+
+        mapBsn.put("bsn_id", id);
+
+        this.daoHelper.update(this.businessDao, attributesValues, mapBsn);
+
+        return this.daoHelper.update(this.agencyGuideDao, agencyMap, keysValues);
     }
 
     @Override
     public EntityResult agencyGuideDelete(Map<String, Object> keysValues) throws OntimizeJEERuntimeException {
-        return this.daoHelper.delete(this.agencyGuideDao, keysValues);
+
+        List <String> qKeys = new ArrayList<String>();
+        qKeys.add("B.bsn_id");
+        EntityResult erAgencyGuide = this.daoHelper.query(this.agencyGuideDao,keysValues, qKeys);
+        Map<String,Object> mapBsn = new HashMap<>();
+
+        ArrayList<Integer> al = (ArrayList<Integer>) erAgencyGuide.get("bsn_id");
+        int id = al.get(0);
+
+        mapBsn.put("bsn_id", id);
+
+        this.daoHelper.delete(this.agencyGuideDao, keysValues);
+
+
+        return this.daoHelper.delete(this.businessDao, mapBsn);
     }
 }
