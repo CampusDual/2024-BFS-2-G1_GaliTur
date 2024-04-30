f4dd6b5b5e065d4e0778e836d41ba329ab0fd338
Daniel Verdes Martinez <dverdesm@gmail.com>
2024-04-30 10:13:17 +0200

    CHANGES ID V6

--- cd2024bfs2g1-model/src/main/resources/db/migration/V6__create_guide_agency_related_tables_and_inserts.sql
+++ cd2024bfs2g1-model/src/main/resources/db/migration/V6__create_guide_agency_related_tables_and_inserts.sql
@@ -1,12 +1,10 @@
 CREATE TABLE gui_language(
-      gui_id int4 NOT NULL,
       gui_l_id serial NOT NULL,
       gui_l_name varchar(50) NOT NULL,
       CONSTRAINT gui_l_id_pk PRIMARY KEY (gui_l_id)
 );
 
 CREATE TABLE gui_zone(
-     gui_id int4 NOT NULL,
      gui_z_id serial NOT NULL,
      gui_z_name varchar(50) NOT NULL,
      CONSTRAINT gui_z_id_pk PRIMARY KEY (gui_z_id)
@@ -20,7 +18,4 @@ CREATE TABLE gui_cities(
 );
 
 
-
-ALTER TABLE gui_language ADD CONSTRAINT gui_l_id_fk FOREIGN KEY (gui_id) REFERENCES bsn_guide_agency(gui_id);
-ALTER TABLE gui_zone ADD CONSTRAINT gui_z_id_fk FOREIGN KEY (gui_id) REFERENCES bsn_guide_agency(gui_id);
 ALTER TABLE gui_cities ADD CONSTRAINT gui_c_id_fk FOREIGN KEY (gui_z_id) REFERENCES gui_zone(gui_z_id);
