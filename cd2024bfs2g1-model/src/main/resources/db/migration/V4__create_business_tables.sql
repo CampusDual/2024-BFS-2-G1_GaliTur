9bbf196473586b407703f1000d5d101415fbc5db
Daniel Verdes Martinez <dverdesm@gmail.com>
2024-05-03 08:47:58 +0200

    Merge V4-V6

--- cd2024bfs2g1-model/src/main/resources/db/migration/V4__create_business_tables.sql
+++ cd2024bfs2g1-model/src/main/resources/db/migration/V4__create_business_tables.sql
@@ -43,7 +43,7 @@ CREATE TABLE bsn_hotel_rooms (
     htl_id int4 NOT NULL,
 	rm_id serial NOT NULL,
 	rm_type varchar(50) NOT NULL,
-	rm_cost decimal NOT NULL,
+	rm_cost decimal(10,2) NOT NULL,
 	CONSTRAINT bsn_hotel_rooms_pk PRIMARY KEY (rm_id)
 );
 
@@ -51,10 +51,62 @@ CREATE TABLE bsn_hotel_services (
     htl_id int4 NOT NULL,
 	srv_id serial NOT NULL,
 	srv_type varchar(50) NOT NULL,
-	srv_cost decimal NOT NULL,
+	srv_cost decimal(10,2) NOT NULL,
 	CONSTRAINT bsn_hotel_services_pk PRIMARY KEY (srv_id)
 );
 
+CREATE TABLE gui_language(
+      gui_l_id serial NOT NULL,
+      gui_l_name varchar(50) NOT NULL,
+      CONSTRAINT gui_l_id_pk PRIMARY KEY (gui_l_id)
+);
+
+CREATE TABLE gui_zone(
+     gui_z_id serial NOT NULL,
+     gui_z_name varchar(50) NOT NULL,
+     CONSTRAINT gui_z_id_pk PRIMARY KEY (gui_z_id)
+);
+
+CREATE TABLE gui_cities(
+     gui_z_id int4 NOT NULL,
+     gui_c_id serial NOT NULL,
+     gui_c_name varchar(50) NOT NULL,
+     CONSTRAINT gui_c_id_pk PRIMARY KEY (gui_c_id)
+);
+
+ALTER TABLE gui_cities ADD CONSTRAINT gui_c_id_fk FOREIGN KEY (gui_z_id) REFERENCES gui_zone(gui_z_id);
+
+
+INSERT INTO public.gui_language
+(gui_l_id, gui_l_name)
+VALUES(nextval('gui_language_gui_l_id_seq'::regclass), 'SPANISH'),
+ (nextval('gui_language_gui_l_id_seq'::regclass), 'GALICIAN'),
+ (nextval('gui_language_gui_l_id_seq'::regclass), 'ENGLISH'),
+ (nextval('gui_language_gui_l_id_seq'::regclass), 'GERMAN'),
+ (nextval('gui_language_gui_l_id_seq'::regclass), 'PORTUGUESE'),
+ (nextval('gui_language_gui_l_id_seq'::regclass), 'FRENCH');
+
+INSERT INTO public.gui_zone
+(gui_z_id, gui_z_name)
+VALUES(nextval('gui_zone_gui_z_id_seq'::regclass), 'CORUNA'),
+(nextval('gui_zone_gui_z_id_seq'::regclass), 'LUGO'),
+(nextval('gui_zone_gui_z_id_seq'::regclass), 'OURENSE'),
+(nextval('gui_zone_gui_z_id_seq'::regclass), 'PONTEVEDRA');
+
+INSERT INTO public.gui_cities
+(gui_z_id, gui_c_id, gui_c_name)
+VALUES(1, nextval('gui_cities_gui_c_id_seq'::regclass), 'CORUNA'),
+(1, nextval('gui_cities_gui_c_id_seq'::regclass), 'FERROL'),
+(1, nextval('gui_cities_gui_c_id_seq'::regclass), 'SANTIAGO'),
+(2, nextval('gui_cities_gui_c_id_seq'::regclass), 'LUGO'),
+(2, nextval('gui_cities_gui_c_id_seq'::regclass), 'MONFORTE'),
+(2, nextval('gui_cities_gui_c_id_seq'::regclass), 'VIVEIRO'),
+(3, nextval('gui_cities_gui_c_id_seq'::regclass), 'OURENSE'),
+(3, nextval('gui_cities_gui_c_id_seq'::regclass), 'VERIN'),
+(3, nextval('gui_cities_gui_c_id_seq'::regclass), 'XINZO'),
+(4, nextval('gui_cities_gui_c_id_seq'::regclass), 'PONTEVEDRA'),
+(4, nextval('gui_cities_gui_c_id_seq'::regclass), 'VIGO'),
+(4, nextval('gui_cities_gui_c_id_seq'::regclass), 'MARIN');
 
 
 ALTER TABLE bsn_business ADD CONSTRAINT bsn_business_fk FOREIGN KEY (merchant_id) REFERENCES merchant(merchant_id);
