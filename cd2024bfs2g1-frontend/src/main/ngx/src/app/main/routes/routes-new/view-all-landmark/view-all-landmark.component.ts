0178da97e1e3efe78287bbd9992b6d0b58ddcfd2
danielsalgadocanosa <danisc2003@gmail.com>
2024-05-09 12:51:19 +0200

    tabla de visualizar puntos de interes

--- cd2024bfs2g1-frontend/src/main/ngx/src/app/main/routes/routes-new/view-all-landmark/view-all-landmark.component.ts
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/main/routes/routes-new/view-all-landmark/view-all-landmark.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Expression, FilterExpressionUtils, Util } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-view-all-landmark',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-all-landmark.component.css']
 })
 export class ViewAllLandmarkComponent {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  createFilter(values: Array<{ attr; value }>): Expression {
+    // Prepare simple expressions from the filter components values
+    let filters: Array<Expression> = [];
+    values.forEach(fil => {
+      if (Util.isDefined(fil.value)) {
+        if (
+          fil.attr === "name" ||
+          fil.attr === "description" ||
+          fil.attr === "opening_time" ||
+          fil.attr === "closing_time" ||
+          fil.attr === "coordinates" 
+        ) {
+          filters.push(
+            FilterExpressionUtils.buildExpressionLike(
+              "name" + fil.attr,
+              fil.value
+            )
+          );
+        }
+
+      }
+    });
+
+    // Build complex expression
+    if (filters.length > 0) {
+      return filters.reduce((exp1, exp2) =>
+        FilterExpressionUtils.buildComplexExpression(
+          exp1,
+          exp2,
+          FilterExpressionUtils.OP_AND
+        )
+      );
+    } else {
+      return null;
+    }
+  }
 
 }
