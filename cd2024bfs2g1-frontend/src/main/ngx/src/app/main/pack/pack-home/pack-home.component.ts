545ae9728fc484b8c0941529faf3a1ad0c45d3f2
Diego Casal Carballal <diegocasal2003@gmail.com>
2024-05-09 12:28:42 +0200

    Added basic pack home structure

--- cd2024bfs2g1-frontend/src/main/ngx/src/app/main/pack/pack-home/pack-home.component.ts
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/main/pack/pack-home/pack-home.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DomSanitizer } from '@angular/platform-browser';
+import { OGridComponent, OntimizeService } from 'ontimize-web-ngx';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-pack-home',
+  selector: 'app-business-home',
   templateUrl: './pack-home.component.html',
   styleUrls: ['./pack-home.component.css']
 })
 export class PackHomeComponent {
+  public showWaitForLongTask = false;
+
+  constructor(
+    private ontimizeService: OntimizeService,
+    protected dialog: MatDialog,
+    protected sanitizer: DomSanitizer,
+    private router: Router,
+  ) {
+    this.ontimizeService.configureService(this.ontimizeService.getDefaultServiceConfiguration("businesses"));
+  }
+  ngOnInit() {
+  }
+
+  public getImageSrc(base64: any): any {
+    return base64 ? this.sanitizer.bypassSecurityTrustResourceUrl('data:image/*;base64,' + base64) : './assets/images/no-image-transparent.png';
+  }
+
+  public openDetail(data: any): void {
+    this.router.navigate(['main/businesses/' + data.bsn_id]);
+  }
+
+  truncateName(name: string): string {
+    if (name.length > 25) {
+        return name.substr(0, 25) + '...';
+    } else {
+        return name;
+    }
+}
 
 }
--- cd2024bfs2g1-frontend/src/main/ngx/src/app/main/pack/pack-routing.module.ts
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/main/pack/pack-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PackHomeComponent } from './pack-home/pack-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: PackHomeComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
