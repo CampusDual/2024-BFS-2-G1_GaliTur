dd705f694a091373653059ecfc957997bef83e34
Simón Souto Dopazo <simonsoutodopazo@gmail.com>
2024-05-09 15:04:23 +0200

    Creación de detalles de puntos de interes

--- cd2024bfs2g1-frontend/src/main/ngx/src/app/main/routes/routes-detail/routes-detail.component.ts
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/main/routes/routes-detail/routes-detail.component.ts
@@ -1,9 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { DomSanitizer } from '@angular/platform-browser';
-import { ImageService } from '../../image.service';
-import { ActivatedRoute, Router } from '@angular/router';
 import { ViewLandmarkDetailComponent } from './view-landmark-detail/view-landmark-detail.component';
+import { LandmarksService } from '../../landmarks.service';
 
 @Component({
   selector: 'app-routes-detail',
@@ -12,26 +11,37 @@ import { ViewLandmarkDetailComponent } from './view-landmark-detail/view-landmar
 })
 export class RoutesDetailComponent implements OnInit{
 
-  galleryImages: any[] = [];
-  landmark: any;
-
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     protected sanitizer: DomSanitizer,
-    protected dialog: MatDialog
+    protected dialog: MatDialog,
+    protected landmarkService: LandmarksService
   ) { }
 
   ngOnInit(){
   }
 
   public openDetailLandmark(data: any): void {
+
+    this.landmarkService.getLandmark(data.route_id).subscribe((landmarkData)=> {
+      const landmark = []
+
+      if(landmarkData.data.length){
+        landmarkData.data.forEach(element => {
+          landmarkData.push(element.name)
+        });
+        data['landmark'] = landmark
+      } 
+
       this.dialog.open(ViewLandmarkDetailComponent, {
         height: '800px',
         width: '1200px',
-        data: data
+        data: data,
       });
-    
+
+    })
   }
 
 }
+
