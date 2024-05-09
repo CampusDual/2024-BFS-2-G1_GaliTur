34984a1cee0a2aa45ccfd65289fdb6b37e0ea4b1
xoanaraujo <xoanaraujodev@gmail.com>
2024-05-09 12:05:53 +0200

    Feat: (V9) pack_state Refactor: (V9) pck_active -> pcs_state

--- cd2024bfs2g1-model/src/main/resources/db/migration/V9__create_pack_table.sql
+++ cd2024bfs2g1-model/src/main/resources/db/migration/V9__create_pack_table.sql
@@ -5,14 +5,25 @@ CREATE TABLE pack
     pck_description  text        NOT NULL,
     pck_date_begin   timestamp   NOT NULL,
     pck_date_end     timestamp   NOT NULL,
-    pck_active       boolean     NOT NULL DEFAULT TRUE,
+    pcs_state        integer     NOT NULL,
     pck_price        decimal(10, 2),
     pck_participants integer,
     gui_c_id         integer     NOT NULL,
     CONSTRAINT pack_pk PRIMARY KEY (pck_id)
 );
 ALTER TABLE pack
+    ADD CONSTRAINT pack_pcs_state_fk FOREIGN KEY (pcs_state) REFERENCES pack_state (pcs_state);
+ALTER TABLE pack
     ADD CONSTRAINT pack_gui_c_id_fk FOREIGN KEY (gui_c_id) REFERENCES gui_cities (gui_c_id);
+
+CREATE TABLE pack_state
+(
+    pcs_id    serial NOT NULL,
+    pcs_state varchar(50),
+    CONSTRAINT pack_state PRIMARY KEY (pcs_id)
+);
+INSERT INTO pack_state VALUES ('active');
+INSERT INTO pack_state VALUES ('reserved');
 
 CREATE TABLE image_pack
 (
@@ -25,3 +36,4 @@ ALTER TABLE image_pack
     ADD CONSTRAINT image_pack_pck_id_fk FOREIGN KEY (pck_id) REFERENCES pack (pck_id);
 ALTER TABLE image_pack
     ADD CONSTRAINT image_pack_img_id_fk FOREIGN KEY (img_id) REFERENCES image (image_id);
+
