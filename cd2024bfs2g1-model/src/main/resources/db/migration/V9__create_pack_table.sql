29ae198c2d662af7b1f140d309b2d75b7135c185
xoanaraujo <xoanaraujodev@gmail.com>
2024-05-09 13:00:41 +0200

    Feat: (V9) pack_state Refactor: (V9) pck_active -> pcs_state

--- cd2024bfs2g1-model/src/main/resources/db/migration/V9__create_pack_table.sql
+++ cd2024bfs2g1-model/src/main/resources/db/migration/V9__create_pack_table.sql
@@ -1,3 +1,8 @@
+CREATE TABLE pack_state
+(
+    pcs_id    serial      NOT NULL PRIMARY KEY,
+    pcs_state varchar(50) NOT NULL
+);
 CREATE TABLE pack
 (
     pck_id           serial      NOT NULL,
@@ -5,35 +10,22 @@ CREATE TABLE pack
     pck_description  text        NOT NULL,
     pck_date_begin   timestamp   NOT NULL,
     pck_date_end     timestamp   NOT NULL,
-    pcs_state        integer     NOT NULL,
     pck_price        decimal(10, 2),
     pck_participants integer,
-    gui_c_id         integer     NOT NULL,
+    gui_c_id         integer     NOT NULL REFERENCES gui_cities (gui_c_id),
+    pcs_id           integer     NOT NULL DEFAULT 1 REFERENCES pack_state (pcs_id),
     CONSTRAINT pack_pk PRIMARY KEY (pck_id)
 );
-ALTER TABLE pack
-    ADD CONSTRAINT pack_pcs_state_fk FOREIGN KEY (pcs_state) REFERENCES pack_state (pcs_state);
-ALTER TABLE pack
-    ADD CONSTRAINT pack_gui_c_id_fk FOREIGN KEY (gui_c_id) REFERENCES gui_cities (gui_c_id);
-
-CREATE TABLE pack_state
-(
-    pcs_id    serial NOT NULL,
-    pcs_state varchar(50),
-    CONSTRAINT pack_state PRIMARY KEY (pcs_id)
-);
-INSERT INTO pack_state VALUES ('active');
-INSERT INTO pack_state VALUES ('reserved');
 
 CREATE TABLE image_pack
 (
     imp_id serial,
-    pck_id integer NOT NULL,
-    img_id integer NOT NULL,
+    pck_id integer NOT NULL REFERENCES pack (pck_id),
+    img_id integer NOT NULL REFERENCES image (image_id),
     CONSTRAINT image_pack_pk PRIMARY KEY (imp_id)
 );
-ALTER TABLE image_pack
-    ADD CONSTRAINT image_pack_pck_id_fk FOREIGN KEY (pck_id) REFERENCES pack (pck_id);
-ALTER TABLE image_pack
-    ADD CONSTRAINT image_pack_img_id_fk FOREIGN KEY (img_id) REFERENCES image (image_id);
 
+INSERT INTO pack_state(pcs_state)
+VALUES ('active');
+INSERT INTO pack_state(pcs_state)
+VALUES ('reserved');
