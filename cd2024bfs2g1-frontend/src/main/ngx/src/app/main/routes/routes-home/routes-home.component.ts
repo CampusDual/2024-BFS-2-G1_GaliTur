b2f022a6b8322fb6f33af5b341a61b5913096a84
Simón Souto Dopazo <simonsoutodopazo@gmail.com>
2024-05-09 12:55:03 +0200

    Creación de detalles de puntos de interes

--- cd2024bfs2g1-frontend/src/main/ngx/src/app/main/routes/routes-home/routes-home.component.ts
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/main/routes/routes-home/routes-home.component.ts
@@ -48,5 +48,26 @@ data: any;
     })
   }
 
+  public convertTime(minutos: number):  string {
+    
+    const horas = Math.floor(minutos / 60);
+    const minutosRestantes = minutos % 60;
+
+    if(horas == 0 && minutosRestantes != 0){
+      return minutosRestantes + "min";
+
+    }else if(horas != 0 && minutosRestantes == 0){
+      return horas + "h ";
+
+    }else{
+       return horas + "h " + minutosRestantes + "min";
+    }
+   
+  
+  }
+
+
+
+
 
 }
