e0a032f1a3aaab707b75d3451c4e99426ea5ed10
Daniel Verdes Martinez <dverdesm@gmail.com>
2024-05-10 09:08:28 +0200

    Feat: pack booking tests

--- cd2024bfs2g1-frontend/src/main/ngx/src/app/main/business/business-detail/business-detail.component.ts
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/main/business/business-detail/business-detail.component.ts
@@ -1,8 +1,8 @@
-import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { Component, Inject, Injector, OnInit, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { OFormComponent, OTableComponent, OntimizeService } from 'ontimize-web-ngx';
+import { DialogService, ODialogConfig, OFormComponent, OTableComponent, OntimizeService } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-business-detail',
@@ -11,17 +11,46 @@ import { OFormComponent, OTableComponent, OntimizeService } from 'ontimize-web-n
 })
 export class BusinessDetailComponent {
 
+ 
+  
+
+
+
+
   @ViewChild('accountCustomerTable') accountTable: OTableComponent;
   @ViewChild('form') form: OFormComponent;
+
 
   constructor(
     protected sanitizer: DomSanitizer,
     private router: Router,
+    protected dialogService: DialogService,
+    protected injector: Injector,
+    protected service: OntimizeService
   ) {
+    this.service = this.injector.get(OntimizeService);
+
   }
+  
 
 	ngOnInit(): void {
+  
+      this.configureService();
+    
   }
+  protected configureService() {
+    // Configure the service using the configuration defined in the `app.services.config.ts` file
+    const conf = this.service.getDefaultServiceConfiguration('packBookings');
+    this.service.configureService(conf);
+  }
+
+  getMovements(data) {
+   
+    this.service.insert({ "pck_id": data.bsn_id }, "packBooking")
+    .subscribe(resp => {
+     this.form.reload(true);
+    });
+    }
 
   public getImageSrc(base64: any): any {
     return base64 ? this.sanitizer.bypassSecurityTrustResourceUrl('data:image/*;base64,' + base64) : './assets/images/no-image-transparent.png';
@@ -30,4 +59,32 @@ export class BusinessDetailComponent {
   public openBusinesses(): void {
     this.router.navigate(['main/businesses']);
   }
+
+  bookPack(event: any, data) {
+
+    const config: ODialogConfig = {
+      icon: 'warning',
+      alertType: 'warn',
+
+    };
+    
+    if (this.dialogService) {
+      this.dialogService.confirm('Confirme reserva del pack', '¿Quiere confirmar la reserva?', config);
+      this.dialogService.dialogRef.afterClosed().subscribe( result => {
+        if(result) {
+          this.getMovements(data);
+
+          // Actions on confirmation
+        } else {
+          // Actions on cancellation
+        }
+      })
+    }
+
+
+  }
+
+
+
+ 
 }
--- cd2024bfs2g1-frontend/src/main/ngx/src/app/shared/app.services.config.ts
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/shared/app.services.config.ts
@@ -43,5 +43,8 @@ export const SERVICE_CONFIG: Object = {
   },
   'guideCities':{
     'path':'/guideCities'
+  },
+  'packBookings':{
+    'path':'/packBookings'
   }
 };
