3f47fb686a569f66f033173d4f8e2d0d388ea710
xoanaraujo <xoanaraujodev@gmail.com>
2024-05-10 09:17:06 +0200

    WIP: Pack endpoints

---
+++ cd2024bfs2g1-model/src/main/java/com/campusdual/cd2024bfs2g1/model/core/dao/pack/PackStateDao.java
@@ -0,0 +1,14 @@
+package com.campusdual.cd2024bfs2g1.model.core.dao.pack;
+
+import com.ontimize.jee.server.dao.common.ConfigurationFile;
+import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+@Lazy
+@Repository("PackStateDao")
+@ConfigurationFile(configurationFile = "dao/pack/PackStateDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
+public class PackStateDao extends OntimizeJdbcDaoSupport {
+    private String PCS_ID = "pcs_id";
+    private String PCS_STATE = "pcs_state";
+}
