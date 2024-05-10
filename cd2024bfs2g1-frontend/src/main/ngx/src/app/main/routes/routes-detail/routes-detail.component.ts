1297c35be2c7bf0c303207f0d1facbdd21145a44
PabloArgs <aragonp27@gmail.com>
2024-05-10 13:10:28 +0200

    Update Routes Home and detail

--- cd2024bfs2g1-frontend/src/main/ngx/src/app/main/routes/routes-detail/routes-detail.component.ts
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/main/routes/routes-detail/routes-detail.component.ts
@@ -19,4 +19,25 @@ export class RoutesDetailComponent {
     protected sanitizer: DomSanitizer,
   ) { }
 
+   ngOnInit() {
+  }
+
+  public convertTime(minutos: number):  string {
+    
+    const horas = Math.floor(minutos / 60);
+    const minutosRestantes = minutos % 60;
+
+    if(horas == 0 && minutosRestantes != 0){
+      return minutosRestantes + "min";
+
+    }else if(horas != 0 && minutosRestantes == 0){
+      return horas + "h ";
+
+    }else{
+       return horas + "h " + minutosRestantes + "min";
+    }
+   
+  
+  }
+
 }
