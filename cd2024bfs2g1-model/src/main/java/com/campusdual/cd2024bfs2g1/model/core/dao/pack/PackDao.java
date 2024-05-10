19b57ca662554c80c1b4969c09fe039b6bce97a9
xoanaraujo <xoanaraujodev@gmail.com>
2024-05-10 10:12:29 +0200

    Feat: Pack insert service

--- cd2024bfs2g1-model/src/main/java/com/campusdual/cd2024bfs2g1/model/core/dao/pack/PackDao.java
+++ cd2024bfs2g1-model/src/main/java/com/campusdual/cd2024bfs2g1/model/core/dao/pack/PackDao.java
@@ -9,13 +9,13 @@ import org.springframework.stereotype.Repository;
 @Repository("PackDao")
 @ConfigurationFile(configurationFile = "dao/pack/PackDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
 public class PackDao extends OntimizeJdbcDaoSupport {
-    private String PCK_ID = "pck_id";
-    private String PCK_NAME = "pck_name";
-    private String PCK_DESCRIPTION = "pck_description";
-    private String PCK_DATE_BEGIN = "pck_date_begin";
-    private String PCK_DATE_END = "pck_date_end";
-    private String PCK_ACTIVE = "pck_active";
-    private String PCK_PRICE = "pck_price";
-    private String PCK_PARTICIPANTS = "pck_participants";
-    private String PCK_GUI_C_ID = "gui_c_id";
+    public static final String PCK_ID = "pck_id";
+    public static final String PCK_NAME = "pck_name";
+    public static final String PCK_DESCRIPTION = "pck_description";
+    public static final String PCK_DATE_BEGIN = "pck_date_begin";
+    public static final String PCK_DATE_END = "pck_date_end";
+    public static final String PCK_ACTIVE = "pck_active";
+    public static final String PCK_PRICE = "pck_price";
+    public static final String PCK_PARTICIPANTS = "pck_participants";
+    public static final String PCK_GUI_C_ID = "gui_c_id";
 }
