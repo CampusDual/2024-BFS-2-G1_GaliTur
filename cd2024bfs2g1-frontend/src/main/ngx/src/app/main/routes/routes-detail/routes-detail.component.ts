e4be22fad53f258d46563705853cc0d80ff9ce7b
Simón Souto Dopazo <simonsoutodopazo@gmail.com>
2024-05-13 11:57:42 +0200

    Cambio del boton en Detail

--- cd2024bfs2g1-frontend/src/main/ngx/src/app/main/routes/routes-detail/routes-detail.component.ts
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/main/routes/routes-detail/routes-detail.component.ts
@@ -1,38 +1,39 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { DomSanitizer } from '@angular/platform-browser';
-import { ImageService } from '../../image.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { LandmarksService } from '../../landmarks.service';
+import { OntimizeService } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-routes-detail',
   templateUrl: './routes-detail.component.html',
   styleUrls: ['./routes-detail.component.css']
 })
-export class RoutesDetailComponent {
-
-  galleryImages: any[] = [];
-
+export class RoutesDetailComponent implements OnInit{
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
+    private ontimizeService: OntimizeService,
     protected sanitizer: DomSanitizer,
-  ) { }
+    protected dialog: MatDialog,
+    protected landmarkService: LandmarksService
+  ) {
+    this.ontimizeService.configureService(this.ontimizeService.getDefaultServiceConfiguration("landmarks"));
+   }
 
-   ngOnInit() {
+  ngOnInit(){
   }
+
+
 
   public convertTime(minutos: number):  string {
     
     const horas = Math.floor(minutos / 60);
     const minutosRestantes = minutos % 60;
-
     if(horas == 0 && minutosRestantes != 0){
       return minutosRestantes + "min";
-
     }else if(horas != 0 && minutosRestantes == 0){
       return horas + "h ";
-
     }else{
        return horas + "h " + minutosRestantes + "min";
     }
@@ -41,3 +42,4 @@ export class RoutesDetailComponent {
   }
 
 }
+
