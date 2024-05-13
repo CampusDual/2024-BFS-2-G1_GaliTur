0bca78da8fe96f65a5a90694f36377e6aa2d7bb4
xoanaraujo <xoanaraujodev@gmail.com>
2024-05-13 10:05:43 +0200

    Fix: V9. Refactor: packInsert endpoint

--- cd2024bfs2g1-model/src/main/java/com/campusdual/cd2024bfs2g1/model/core/service/pack/PackService.java
+++ cd2024bfs2g1-model/src/main/java/com/campusdual/cd2024bfs2g1/model/core/service/pack/PackService.java
@@ -1,6 +1,7 @@
 package com.campusdual.cd2024bfs2g1.model.core.service.pack;
 
 import com.campusdual.cd2024bfs2g1.api.core.service.pack.IPackService;
+import com.campusdual.cd2024bfs2g1.api.core.util.Utils;
 import com.campusdual.cd2024bfs2g1.model.core.dao.ImageDao;
 import com.campusdual.cd2024bfs2g1.model.core.dao.pack.ImagePackDao;
 import com.campusdual.cd2024bfs2g1.model.core.dao.pack.PackDao;
@@ -15,8 +16,11 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 @Lazy
 @Service("PackService")
@@ -42,9 +46,14 @@ public class PackService implements IPackService {
     @Override
     @Secured(PermissionsProviderSecured.SECURED)
     @Transactional(rollbackFor = Throwable.class)
-    public EntityResult packInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
+    public EntityResult packInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException, ParseException {
         Object imgCode = attrMap.get(ImageDao.ATTR_IMAGE_CODE);
         attrMap.remove(ImageDao.ATTR_IMAGE_CODE);
+
+        Map<String, Object> dates = (Map<String, Object>) attrMap.get("dates");
+        attrMap.remove("dates");
+        attrMap.put(PackDao.PCK_DATE_BEGIN, Utils.iso8601Format.parse((String) dates.get("startDate")));
+        attrMap.put(PackDao.PCK_DATE_END, Utils.iso8601Format.parse((String) dates.get("endDate")));
 
         EntityResult erInsertPack = this.daoHelper.insert(this.packDao, attrMap);
         if (erInsertPack.getCode() != EntityResult.OPERATION_SUCCESSFUL || imgCode == null) return erInsertPack;
