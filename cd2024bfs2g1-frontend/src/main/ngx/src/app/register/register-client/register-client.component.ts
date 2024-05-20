4fdb5196b07fd1fd72089f52fcbeaad3c571bf77
xoanaraujo <xoanaraujodev@gmail.com>
2024-05-20 14:37:35 +0200

    Fix: username regex

--- cd2024bfs2g1-frontend/src/main/ngx/src/app/register/register-client/register-client.component.ts
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/register/register-client/register-client.component.ts
@@ -146,7 +146,7 @@ export class RegisterClientComponent{
 
   usernameCharsValidator(control: AbstractControl): ValidationErrors | null{
     try{
-        const regex = /^[A-Za-z_\-.]*$/
+        const regex = /^[A-Za-z_\-.\d]*$/
       const inputValue = control.value;
 
       if(regex.test(inputValue)){
