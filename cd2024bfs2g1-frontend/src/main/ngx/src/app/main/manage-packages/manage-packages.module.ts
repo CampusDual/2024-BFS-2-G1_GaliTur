f82cde05a7eb8119f9b011104fa4f99ec09fcceb
GabrielVazquezRodriguez <eficacia743@gmail.com>
2024-05-23 09:21:31 +0200

    creacion de managepacks

---
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/main/manage-packages/manage-packages.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ManagePackagesRoutingModule } from './manage-packages-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ManagePackagesRoutingModule
+  ]
+})
+export class ManagePackagesModule { }
---
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/main/manage-packages/pack-edit/pack-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pack-edit',
+  templateUrl: './pack-edit.component.html',
+  styleUrls: ['./pack-edit.component.css']
+})
+export class PackEditComponent {
+
+}
