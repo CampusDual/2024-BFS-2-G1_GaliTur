d6a0f09a1093a307e03bfc1185d78f945f8b04a3
GabrielVazquezRodriguez <eficacia743@gmail.com>
2024-05-27 08:09:36 +0200

    edit detail

--- cd2024bfs2g1-frontend/src/main/ngx/src/app/main/manage-packages/pack-edit/pack-edit.component.ts
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/main/manage-packages/pack-edit/pack-edit.component.ts
@@ -9,15 +9,12 @@ import { OntimizeService } from 'ontimize-web-ngx';
 })
 export class PackEditComponent {
 
-  constructor(private ontimizeManagePackService: OntimizeService,
-    private ontimizeManagePackImageService: OntimizeService,
-    private activeRoute: ActivatedRoute
-  ){
+  constructor(private ontimizeManagePackService: OntimizeService,private ontimizeManagePackImageService: OntimizeService,
+    private activeRoute: ActivatedRoute){
     this.idPack = this.getPackId()
     this.configurePackImageService()
      this. consultarDatosPackImage(this.idPack)
      console.log("El id de la ruta es:" + this.idPack)
-    
   }
 
   dataPack:any= []
@@ -27,21 +24,15 @@ export class PackEditComponent {
   // } ={}
 
   consultarDatosPackImage(id:number){
-    this.ontimizeManagePackImageService
-      .query(
-        {pck_id:id},
-        ["img_id"],
-        "imagePack"
-      )
-      .subscribe((response) => {
+    this.ontimizeManagePackImageService.query({pck_id:id},["img_id"],"imagePack").subscribe((response) => 
+      {
         this.dataPack.push(...response.data);
         console.log(this.dataPack[0].img_id);
         this.imgId=this.dataPack[0].img_id
       });
   }
   protected configurePackImageService() {
-    const conf =
-      this.ontimizeManagePackImageService.getDefaultServiceConfiguration("imagePacks");
+    const conf = this.ontimizeManagePackImageService.getDefaultServiceConfiguration("imagePacks");
     this.ontimizeManagePackImageService.configureService(conf);
   }
 
