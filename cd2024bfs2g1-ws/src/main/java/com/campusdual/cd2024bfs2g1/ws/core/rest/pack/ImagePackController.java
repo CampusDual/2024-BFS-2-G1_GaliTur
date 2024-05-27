2b96f817ea7f218f9227e3a31008a221963723c9
GabrielVazquezRodriguez <eficacia743@gmail.com>
2024-05-27 08:12:49 +0200

    edit detail

--- cd2024bfs2g1-ws/src/main/java/com/campusdual/cd2024bfs2g1/ws/core/rest/pack/ImagePackController.java
+++ cd2024bfs2g1-ws/src/main/java/com/campusdual/cd2024bfs2g1/ws/core/rest/pack/ImagePackController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/imagePacks")
-public class ImagePackController extends ORestController<IImagePackService> {
+public class ImagePackController extends ORestController<IImagePackService>  {
 
     @Autowired
     private IImagePackService imagePackService;
