56e59fa5ee99c1d16c2485f1b1098fc508e12e8d
DeadSerker <cesar19971@hotmail.es>
2024-05-27 13:40:54 +0200

    create pack date and edit pack,pcs,booking :0

---
+++ cd2024bfs2g1-model/src/main/resources/db/migration/V14__create_pack_date_edit_pack_and_state.sql
@@ -0,0 +1,50 @@
+ALTER TABLE PACK
+DROP COLUMN pck_date_begin;
+
+ALTER TABLE PACK
+DROP COLUMN pck_date_end;
+
+ALTER TABLE PACK
+DROP COLUMN province;
+
+ALTER TABLE PACK
+DROP COLUMN pcs_id;
+
+ALTER TABLE PACK
+ADD COLUMN province varchar(50);
+
+ALTER TABLE PACK
+DROP CONSTRAINT pack_pcs_id_fkey;
+
+CREATE TABLE pack_date (
+    pd_id SERIAL PRIMARY KEY NOT NULL,
+    pd_date_begin TIMESTAMP NOT NULL,
+    pd_date_end TIMESTAMP NOT NULL,
+    pcs_id INT NOT NULL DEFAULT 1,
+    pck_id INT NOT NULL,
+    CONSTRAINT fk_pack_state
+        FOREIGN KEY(pcs_id)
+            REFERENCES pack_state(pcs_id),
+    CONSTRAINT fk_pack
+        FOREIGN KEY(pck_id)
+            REFERENCES pack(pck_id)
+);
+
+INSERT INTO pack_state(pcs_state)
+VALUES ('active');
+INSERT INTO pack_state(pcs_state)
+VALUES ('reserved');
+
+ALTER TABLE pack_booking
+DROP CONSTRAINT pck_id_fk;
+
+ALTER TABLE pack_booking
+DROP COLUMN pck_id;
+
+ALTER TABLE pack_booking
+ADD COLUMN pd_id INT;
+
+ALTER TABLE pack_booking
+ADD CONSTRAINT fk_pack_date
+FOREIGN KEY (pd_id)
+REFERENCES pack_date(pd_id);
