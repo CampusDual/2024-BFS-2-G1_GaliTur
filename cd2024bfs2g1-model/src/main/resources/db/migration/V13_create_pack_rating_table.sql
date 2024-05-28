73a226794b60558b9a02cc1a6adf9a64ec8b1a79
xoanaraujo <xoanaraujodev@gmail.com>
2024-05-28 12:01:49 +0200

    Feat: V13_create_pack_rating_table

---
+++ cd2024bfs2g1-model/src/main/resources/db/migration/V13_create_pack_rating_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE pack_rating
+(
+    pcr              SERIAL PRIMARY KEY NOT NULL,
+    pck_id           int4               not NULL,
+    client_id        int4               not NULL,
+    pcr_stars        int2               not NULL CHECK ( pcr_stars BETWEEN 1 AND 5),
+    pcr_comment      varchar(255)
+);
