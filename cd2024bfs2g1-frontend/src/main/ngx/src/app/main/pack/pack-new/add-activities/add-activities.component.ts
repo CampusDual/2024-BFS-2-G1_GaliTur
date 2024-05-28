de31119cb4c701e9d356f7beaf2997535d214ce4
Daniel Verdes Martinez <dverdesm@gmail.com>
2024-05-28 12:51:21 +0200

    Feat: list bsn_packs by day

--- cd2024bfs2g1-frontend/src/main/ngx/src/app/main/pack/pack-new/add-activities/add-activities.component.ts
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/main/pack/pack-new/add-activities/add-activities.component.ts
@@ -1,8 +1,8 @@
-import { Component, ViewChild } from "@angular/core";
+import { Component, Injector, ViewChild } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { PackActivitiesComponent } from "./pack-activities/pack-activities.component";
 import { ActivatedRoute } from "@angular/router";
-import { FilterExpressionUtils, OTableComponent } from "ontimize-web-ngx";
+import { FilterExpressionUtils, OTableComponent, OntimizeService } from "ontimize-web-ngx";
 
 @Component({
   selector: "app-add-activities",
@@ -12,9 +12,10 @@ import { FilterExpressionUtils, OTableComponent } from "ontimize-web-ngx";
 export class AddActivitiesComponent {
   @ViewChild("table", { static: false }) table: OTableComponent;
   static packId: any;
+  protected service: OntimizeService;
+  public bsnArray = []
 
-filterExpr = FilterExpressionUtils.buildExpressionLike('pck_id', AddActivitiesComponent.packId);
-basicExpr = FilterExpressionUtils.buildBasicExpression(this.filterExpr);
+
 
 
 
@@ -22,13 +23,16 @@ basicExpr = FilterExpressionUtils.buildBasicExpression(this.filterExpr);
   constructor(
     protected dialog: MatDialog,
     private activeRoute: ActivatedRoute,
-    
+    protected injector: Injector
   ) {
-    this.table.queryData(this.basicExpr);
+    this.service = this.injector.get(OntimizeService);
   }
   ngOnInit(): void {
     AddActivitiesComponent.packId = this.activeRoute.snapshot.params["pck_id"];
+    this.configureService();
+
     console.log("Al pack le llego el id: " + AddActivitiesComponent.packId);
+    this.getBsn();
   }
 
   addActivity() {
@@ -37,6 +41,30 @@ basicExpr = FilterExpressionUtils.buildBasicExpression(this.filterExpr);
       maxHeight: "600px",
       minHeight: "600px",
       maxWidth: "80vw",
+    });
+  }
+
+  protected configureService() {
+    // Configure the service using the configuration defined in the `app.services.config.ts` file
+    const conf = this.service.getDefaultServiceConfiguration("packs");
+    this.service.configureService(conf);
+  }
+
+  getBsn() {
+    const filter = {
+      "BP.pck_id": parseInt(AddActivitiesComponent.packId) ,
+    };
+    const columns = ["bsn_name", "bsn_type","bsn_address","assigned_date","bsn_pack_id"];
+    this.service.query(filter, columns, "packBusiness").subscribe((resp) => {
+      if (resp.code === 0) {
+        // resp.data contains the data retrieved from the server
+
+        this.table.setDataArray(resp.data);
+
+       
+      } else {
+        alert("Impossible to query data!");
+      }
     });
   }
 }
