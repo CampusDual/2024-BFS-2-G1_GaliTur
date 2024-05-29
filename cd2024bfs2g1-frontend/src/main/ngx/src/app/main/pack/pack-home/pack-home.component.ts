bee86f21f568cde4a6ee5fa3d7e0d3356bc2c355
dariorigueira <dariorigueiram@gmail.com>
2024-05-29 13:27:02 +0200

    Fixed packs-home query bug. Filters working.

--- cd2024bfs2g1-frontend/src/main/ngx/src/app/main/pack/pack-home/pack-home.component.ts
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/main/pack/pack-home/pack-home.component.ts
@@ -67,70 +67,42 @@ export class PackHomeComponent {
     return tempFecha.toLocaleDateString();
   }
 
-  filter(values: Array<{ attr: string, value: any }>): Expression {
+  //FILTROS
+  createFilter(values: Array<{ attr: string, value: any }>): Expression {
     let filters: Array<Expression> = [];
+
     values.forEach(fil => {
-        if (fil.value) {
-            if (fil.attr === 'pck_name') {
-                let keyword = fil.value;
-                filters.push(FilterExpressionUtils.buildExpressionLike("pck_name", `%${keyword}%`));
-            }
-            if (fil.attr === 'pck_date_begin') {
-              let dateBegin = fil.value;
-              filters.push(FilterExpressionUtils.buildExpressionMoreEqual("pck_date_begin", new Date(dateBegin).toISOString().slice(0, 10)));
-            }
-            if (fil.attr === 'pck_date_end') {
-                let dateEnd = fil.value;
-                filters.push(FilterExpressionUtils.buildExpressionLessEqual("pck_date_end", new Date(dateEnd).toISOString().slice(0, 10)));
-            }
-            if (fil.attr === 'pck_days') {
-              let days = Number(fil.value);
-              filters.push(FilterExpressionUtils.buildExpressionEquals(
-                  `DATE_PART('day', DATE_TRUNC('day', pck_date_end) - DATE_TRUNC('day', pck_date_begin))`,
-                  days
-              ));
-          }
-            if (fil.attr === 'pck_price_min') {
-                let value: number = Number(fil.value);
-                filters.push(FilterExpressionUtils.buildExpressionMoreEqual("pck_price", value));
-            }
-            if (fil.attr === 'pck_price_max') {
-                let value: number = Number(fil.value);
-                filters.push(FilterExpressionUtils.buildExpressionLessEqual("pck_price", value));
-            }
-            if (fil.attr === 'pck_participants') {
-                filters.push(FilterExpressionUtils.buildExpressionEquals("pck_participants", fil.value));
-            }
-            if (fil.attr === 'gui_c_name') {
-                filters.push(FilterExpressionUtils.buildExpressionEquals("gui_c_name", fil.value));
-            }
+      if (fil.value) {
+        if (fil.attr === 'pck_name') {
+          let keyword = fil.value;
+          filters.push(FilterExpressionUtils.buildExpressionLike("pck_name", `%${keyword}%`));
         }
+        if (fil.attr === 'pck_days') {
+          filters.push(FilterExpressionUtils.buildExpressionMoreEqual("pck_days", fil.value));
+        }
+        if (fil.attr === 'pck_price_min') {
+          let value: number = Number(fil.value);
+          filters.push(FilterExpressionUtils.buildExpressionMoreEqual("pck_price", value));
+        }
+        if (fil.attr === 'pck_price_max') {
+          let value: number = Number(fil.value);
+          filters.push(FilterExpressionUtils.buildExpressionLessEqual("pck_price", value));
+        }
+        if (fil.attr === 'pck_participants') {
+          filters.push(FilterExpressionUtils.buildExpressionEquals("pck_participants", fil.value));
+        }
+        if (fil.attr === 'gui_c_name') {
+          filters.push(FilterExpressionUtils.buildExpressionEquals("gui_c_name", fil.value));
+        }
+      }
     });
 
     if (filters.length > 0) {
-        return filters.reduce((exp1, exp2) => 
-            FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND)
-        );
+      return filters.reduce((exp1, exp2) => 
+        FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND)
+      );
     } else {
-        return null;
+      return null;
     }
-  }
-
-  showFilter(evt: any) {
-    this.dialogRef = this.dialog.open(this.filterDialogTemplate, {
-      width: '50%',
-      height: '50%',
-    });
-  }
-
-  applyFilter() {
-    const filterBuilder = this.filterDialogTemplate['filterBuilder'];
-    const filters = filterBuilder.getExpression();
-    this.packGrid.queryData(filters);
-    this.dialogRef.close();
-  }
-
-  closeDialog() {
-    this.dialogRef.close();
   }
 }
