71f2f884d759ed60087ae723f51803b60598cb53
xoanaraujo <xoanaraujodev@gmail.com>
2024-05-30 12:07:27 +0200

    WIP

--- cd2024bfs2g1-frontend/src/main/ngx/src/app/main/pack/pack-detail/pack-detail.component.ts
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/main/pack/pack-detail/pack-detail.component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject, Injector, OnInit, ViewChild} from "@angular/core";
-import { DomSanitizer } from "@angular/platform-browser";
+import {DomSanitizer} from "@angular/platform-browser";
 import {ActivatedRoute, Router} from "@angular/router";
 import {
   DialogService,
@@ -9,20 +9,23 @@ import {
   OTableComponent,
   OTranslateService,
   OntimizeService,
-  SnackBarService, AuthService, OComboComponent,
+  SnackBarService, AuthService, OComboComponent, OGridComponent,
 } from "ontimize-web-ngx";
-import { PackHomeComponent } from "../pack-home/pack-home.component";
+import {PackHomeComponent} from "../pack-home/pack-home.component";
+import {UserInfoService} from "../../../shared/services/user-info.service";
 
 @Component({
   selector: "app-pack-detail",
   templateUrl: "./pack-detail.component.html",
   styleUrls: ["./pack-detail.component.css"],
 })
-export class PackDetailComponent  implements OnInit{
+export class PackDetailComponent implements OnInit {
   @ViewChild("form") formPack: OFormComponent
-  @ViewChild("comboPackDate") comboPackDate: OComboComponent
+  @ViewChild("packDatesForm") packDatesForm: OGridComponent
+  @ViewChild("packDateCombo") packDateCombo: OComboComponent
   protected isPackInstance: boolean
   protected availableDates: Set<any> = new Set
+
   constructor(
     protected sanitizer: DomSanitizer,
     private router: Router,
@@ -33,7 +36,8 @@ export class PackDetailComponent  implements OnInit{
     protected injector: Injector,
     protected bookingService: OntimizeService,
     protected snackBarService: SnackBarService,
-    @Inject(AuthService) private authService: AuthService
+    @Inject(AuthService) private authService: AuthService,
+    @Inject(UserInfoService) private userInfoService: UserInfoService
   ) {
     this.bookingService = this.injector.get(OntimizeService);
   }
@@ -42,15 +46,16 @@ export class PackDetailComponent  implements OnInit{
     // this.isPackInstance = false
     // this.isInstanceOfPack()
   }
-  ngAfterViewInit(){
-    // this.populateDates()
+
+  ngAfterViewInit() {
+    this.populateDates()
   }
 
   public getImageSrc(base64: any): any {
     return base64
       ? this.sanitizer.bypassSecurityTrustResourceUrl(
-          "data:image/*;base64," + base64
-        )
+        "data:image/*;base64," + base64
+      )
       : "./assets/images/no-image-transparent.png";
   }
 
@@ -102,7 +107,7 @@ export class PackDetailComponent  implements OnInit{
     const confBooking = this.bookingService.getDefaultServiceConfiguration("packBookings");
     this.bookingService.configureService(confBooking);
     this.bookingService
-      .insert({ pck_id: data.pck_id }, "packBooking")
+      .insert({pd_id: data.pd_id, client_id: this.userInfoService.getUserInfo().usr_id}, "packBooking")
       .subscribe((resp) => {
         //TODO: this.form.reload(true);
 
@@ -121,25 +126,29 @@ export class PackDetailComponent  implements OnInit{
     return this.authService.isLoggedIn()
   }
 
-  private populateDates() {
+  populateDates() {
     const confPack = this.packDateService.getDefaultServiceConfiguration('packDates');
     this.packDateService.configureService(confPack);
 
-    const id= +this.route.snapshot.params["pck_id"]
+    const id = +this.route.snapshot.params["pck_id"]
 
     this.packDateService.query(
       {pck_id: id},
       ["pd_id", "pd_date_begin", "pd_date_end"],
       "packDate",
       {
-        pd_id:4,
-        pd_date_begin:93,
-        pd_date_end:93
+        pd_id: 4,
+        pd_date_begin: 93,
+        pd_date_end: 93
       })
       .subscribe((result) => {
-        if (result.data[0] !== undefined){
-          this.comboPackDate.setDataArray(result.data)
-          console.log(this.availableDates)
+        if (result.data.length) {
+          result.data.forEach((date) => {
+            date.pd_date_begin = new Date(date.pd_date_begin).toLocaleDateString()
+            date.pd_date_end = new Date(date.pd_date_end).toLocaleDateString()
+          })
+          this.packDateCombo.setDataArray(result.data)
+          // console.log(this.availableDates)
         }
       });
   }
