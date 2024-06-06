aaead2692c82d7c49ae05cc5076c4069180b6734
dariorigueira <dariorigueiram@gmail.com>
2024-06-06 09:03:24 +0200

    Popular packs query and data fetching works.

--- cd2024bfs2g1-frontend/src/main/ngx/src/app/main/home/home.component.ts
+++ cd2024bfs2g1-frontend/src/main/ngx/src/app/main/home/home.component.ts
@@ -7,6 +7,7 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  sanitizer: any;
 
   constructor(
     private router: Router,
@@ -32,4 +33,25 @@ export class HomeComponent implements OnInit {
   public getContentImageSrc(): any {
     return "./assets/images/home-image.jpeg";
   }
+
+  public openDetail(data: any): void {
+    this.router.navigate(['packs/' + data.pck_id]);
+  }
+
+  public getImageSrc(base64: any): any {
+    return base64
+      ? this.sanitizer.bypassSecurityTrustResourceUrl(
+          "data:image/*;base64," + base64
+        )
+      : "./assets/images/no-image-transparent.png";
+  }
+
+  truncateName(name: string): string {
+    return name.length > 25 ? name.substr(0, 25) + "..." : name;
+  }
+
+  truncateInfo(name: string): string {
+    return name.length > 10 ? name.substr(0, 10) + "..." : name;
+  }
+
 }
